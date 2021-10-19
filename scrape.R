5848a876324ada017c7a4b2e44f4258ba823d6ab
Martijn van Bloois <martijn.vanbloois@farminform.nl>
2021-10-19 19:40:03 +0200

    New script!

--- scrape.R
+++ scrape.R
@@ -1,8 +1,13 @@
-library(rtweet)
-library(magrittr)
-library(readr)
+library(rvest)
 
-filename <- paste0("data/", Sys.Date(), "_tweets_sfknieuws.csv")
+end_date <- as.character(format(Sys.Date(), "%d-%m-%Y"))
+start_date <- as.character(format(Sys.Date() - 31, "%d-%m-%Y"))
+csv <- paste0("data/", end_date, "_cbg_nieuwe-registraties.csv")
 
-get_timeline(user = "SFKnieuws") %>%
-  write_csv(filename)
+url <- paste0("https://www.geneesmiddeleninformatiebank.nl/ords/f?p=111:2:0:NEWRVG:NO:2:P2_AS_APPDATS,P2_AS_APPDATE,P2_OPTIONS,P2_RESPAGE,P2_SORT,P2_RESPPG:",
+  start_date, ",", end_date, ",,1,PRODA,100")
+
+page <- read_html(url)
+
+write.csv(html_table(page)[[1]], csv)
+
