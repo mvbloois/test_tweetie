a9fdbe3570245b83f696ece84f5de85ba163d91b
Martijn van Bloois <martijn.vanbloois@farminform.nl>
2021-10-21 14:07:29 +0200

    Changed date in filename to ISO format.

--- scrape.R
+++ scrape.R
@@ -2,7 +2,8 @@ library(rvest)
 
 end_date <- as.character(format(Sys.Date(), "%d-%m-%Y"))
 start_date <- as.character(format(Sys.Date() - 31, "%d-%m-%Y"))
-csv <- paste0("data/", end_date, "_cbg_nieuwe-registraties.csv")
+csv_date <- as.character(format(Sys.Date(), "%Y-%m-%d"))
+csv <- paste0("data/", csv_date, "_cbg_nieuwe-registraties.csv")
 
 url <- paste0("https://www.geneesmiddeleninformatiebank.nl/ords/f?p=111:2:0:NEWRVG:NO:2:P2_AS_APPDATS,P2_AS_APPDATE,P2_OPTIONS,P2_RESPAGE,P2_SORT,P2_RESPPG:",
   start_date, ",", end_date, ",,1,PRODA,100")
@@ -10,4 +11,3 @@ url <- paste0("https://www.geneesmiddeleninformatiebank.nl/ords/f?p=111:2:0:NEWR
 page <- read_html(url)
 
 write.csv(html_table(page)[[1]], csv)
-
