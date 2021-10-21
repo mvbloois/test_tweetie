7fcfe1cb2a62b54c8600f7d22608130e870520ea
Martijn van Bloois <martijn.vanbloois@farminform.nl>
2021-10-21 14:47:43 +0200

    Corrected status row.names in write.csv Added original URL

--- scrape.R
+++ scrape.R
@@ -1,5 +1,11 @@
 library(rvest)
 
+# Nieuw ingeschreven geneesmiddelen
+# https://www.geneesmiddeleninformatiebank.nl/ords/
+# f?p=111:2:0:NEWRVG:NO:2:P2_AS_APPDATS,P2_AS_APPDATE,
+# P2_OPTIONS,P2_RESPAGE,P2_SORT,P2_RESPPG:
+# 06-09-2021,06-10-2021,,1,PRODA,10
+
 end_date <- as.character(format(Sys.Date(), "%d-%m-%Y"))
 start_date <- as.character(format(Sys.Date() - 31, "%d-%m-%Y"))
 csv_date <- as.character(format(Sys.Date(), "%Y-%m-%d"))
@@ -10,4 +16,4 @@ url <- paste0("https://www.geneesmiddeleninformatiebank.nl/ords/f?p=111:2:0:NEWR
 
 page <- read_html(url)
 
-write.csv(html_table(page)[[1]], csv)
+write.csv(html_table(page)[[1]], csv, row.names = FALSE)
